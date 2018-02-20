11de6e3dfe852522f850677202ec3ffef96bab00
ibrahim_abdelkader <ibrahim.abdelkader@esprit.tn>
2018-02-20 03:28:04 +0100

    final server file added , this file will call call time function for everyday's pool time. with promise method.

---
+++ Server JS/Sprint_3/server.js
@@ -0,0 +1,17 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+var semaine = ["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"]
+var slot =["00:00","08:00","12:00","14:00","18:00","23:59"]
+async function populate(jour,start_time, end_time) {
+  const { stdout, stderr } = await exec('nodejs CallTime.js '+jour+' '+start_time+' '+end_time);
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+}
+semaine.forEach(jour => {
+    for (let index = 0; index < slot.length; index++) {
+        if(slot[index]!="23:59")
+        {
+            populate(jour,slot[index],slot[index+1])
+        }
+    }
+});
