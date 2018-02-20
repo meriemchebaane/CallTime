65f139a7b82c814f53b8179239466d6c1794f529
ibrahim_abdelkader <ibrahim.abdelkader@esprit.tn>
2018-02-20 03:29:26 +0100

    server replaced with index, same functionalities!

--- Server JS/Sprint_3/index.js
+++ Server JS/Sprint_3/index.js
@@ -1,6 +1,6 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-var semaine = ["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"]
+var semaine = ["lundi","mardi","merecredi","jeudi","vendredi","samedi","dimanche"]
 var slot =["00:00","08:00","12:00","14:00","18:00","23:59"]
 async function populate(jour,start_time, end_time) {
   const { stdout, stderr } = await exec('nodejs CallTime.js '+jour+' '+start_time+' '+end_time);
@@ -8,10 +8,15 @@ async function populate(jour,start_time, end_time) {
   console.log('stderr:', stderr);
 }
 semaine.forEach(jour => {
+    //setTimeout(populate_day, 30000);
+  
     for (let index = 0; index < slot.length; index++) {
         if(slot[index]!="23:59")
         {
-            populate(jour,slot[index],slot[index+1])
+          
+                populate(jour,slot[index],slot[index+1]);
+           
+            
         }
     }
 });
